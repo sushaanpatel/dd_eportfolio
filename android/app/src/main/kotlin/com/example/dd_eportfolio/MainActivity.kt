0f3dccd8f22d39a63c41ddb087b0ffff43a708b2
sushaanpatel <sushaanpatel@gmail.com>
2021-12-08 11:33:31 +0530

    Adding inital files

---
+++ android/app/src/main/kotlin/com/example/dd_eportfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dd_eportfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
